25175ca70f4b8e250431ab90784b37f036d9318b
kid782 <stefanbeljin@gmail.com>
2021-10-18 20:23:15 +0200

    Changed code to match new html classes

--- etch.js
+++ etch.js
@@ -1,5 +1,5 @@
 let defaultSize = 16; 
-let color = '#000000';
+let color = '#ff0000';
 let randomColor = Math.floor(Math.random()*16777215).toString(16);
 const grid = document.querySelector(".grid"); 
 const randomColors = document.querySelector("#random");
@@ -7,8 +7,8 @@ const chosenColor = document.querySelector("#favcolor");
 const eraser = document.querySelector("#eraser"); 
 const clearGrid = document.querySelector("#clear");
 const sizeSlider = document.querySelector("#sizeSlider");
-const sliderNumber = document.querySelector("#sliderNumber");
-sliderNumber.textContent = sliderNumber.value;
+const sliderNumber = document.querySelector(".velicina");
+sliderNumber.textContent=`Grid size: ${defaultSize}x${defaultSize}  `;
 
 
 napraviGrid(defaultSize);
@@ -18,7 +18,7 @@ ukljuciCrtanje();
 
 clearGrid.addEventListener("click",()=> ocistiPapir());
 
-eraser.addEventListener("click",()=>koristiBoju('#FFFFFF'));
+eraser.addEventListener("click",()=>koristiBoju('rgb(229, 226, 226)'));
 
 chosenColor.addEventListener("input",()=>koristiBoju(`${chosenColor.value}`));
 
@@ -32,11 +32,11 @@ function napraviGrid(size){
     gridItem.classList.add("grid-item");
 
     grid.appendChild(gridItem); }}
-    
+
 
 function promeniGrid(newSize){ 
   
-    sliderNumber.textContent = sizeSlider.value;
+    sliderNumber.textContent = `Grid size: ${sizeSlider.value}x${sizeSlider.value}  `;
     grid.style.cssText = (`grid-template-columns:repeat(${newSize},1fr);
                grid-template-rows:repeat(${newSize},1fr);`);
     ocistiPapir();
@@ -66,7 +66,7 @@ function randomBoje()
 
 function ocistiPapir(){ 
    gridItems.forEach((gridItem)=> 
-   gridItem.style.background="#FFFFFF")
+   gridItem.style.background="rgb(229, 226, 226)")
 }
 
 
